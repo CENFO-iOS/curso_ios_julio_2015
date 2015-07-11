fc38822a8312ce6291b1f282337ae6100e29b9d0
Simon Cascante <scascacha@gmail.com>
2015-07-11 15:54:12 -0600

    Initial Commit

---
+++ CENFO Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CENFO Movies
+//
+//  Created by Simon Cascante on 7/11/15.
+//  Copyright (c) 2015 CENFOTEC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CENFO Movies/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CENFO Movies
+//
+//  Created by Simon Cascante on 7/11/15.
+//  Copyright (c) 2015 CENFOTEC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
